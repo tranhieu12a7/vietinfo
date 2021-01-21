a2849b2be2d3c7ee4fb32f65cb8aad0499fadd3e
tranhieu12a7 <45960374+tranhieu12a7@users.noreply.github.com>
2021-01-21 15:00:37 +0700

    hieut

--- vietinfo_dev_core/lib/widgets/widget_screen.dart
+++ vietinfo_dev_core/lib/widgets/widget_screen.dart
@@ -3,19 +3,20 @@ import 'package:flutter/material.dart';
 class WidgetScreen extends StatelessWidget {
   final Widget child;
   final Widget appBar;
+  final BoxDecoration appBarBoxDecoration;
   double heightAppBar;
 
-  WidgetScreen({Key key, this.appBar, this.child, this.heightAppBar})
+  WidgetScreen({Key key, this.appBar, this.child, this.heightAppBar,this.appBarBoxDecoration})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     double _heightAppBar =heightAppBar??50 + MediaQuery.of(context).padding.top;
     return Scaffold(
-      body: SizedBox(
+      body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
+        decoration: appBarBoxDecoration??new BoxDecoration(),
         child: Column(
           children: [
             appBar!=null? Container(
